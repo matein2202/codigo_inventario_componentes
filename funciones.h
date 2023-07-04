badb1532095b1984ec23df990a9fed0543958905
matein2202 <130107080+matein2202@users.noreply.github.com>
2023-07-04 12:23:48 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,3 @@
+void verProductos(char procesador[], float precioProcesador, char memoriaRAM[], float precioRAM, char grafica[], float precioGrafica, char tarjetaMadre[], float precioTarjetaMadre);
+void modificarProductos(float *precioProcesador, float *precioRAM, float *precioGrafica, float *precioTarjetaMadre);
+
